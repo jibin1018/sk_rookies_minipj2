5115d5b6e355630b1c4162c22e37902bb3d82155
jibin <wlqls1019@gmail.com>
2025-12-28 23:26:16 +0900

    초기 구현

--- backend/src/main/java/com/company/portal/PortalApplication.java
+++ backend/src/main/java/com/company/portal/PortalApplication.java
@@ -2,12 +2,14 @@ package com.company.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PortalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
 
 }
