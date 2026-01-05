7404a47b24610a68265bca16513e341ff8dbb420
박지빈 <wlqls1019@gmail.com>
2026-01-05 11:26:14 +0900

    파일 위치 수정

---
+++ Scanner/modules/whitebox/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+화이트박스 보안 취약점 스캐너 모듈
+"""
+
+__version__ = '1.0.0'
+__all__ = [
+    'access_control',
+    'injection',
+    'xss_output',
+    'session_management',
+    'secrets_crypto',
+    'file_handling',
+    'deserialization',
+    'logging_errors',
+    'security_headers',
+    'dependencies',
+]
